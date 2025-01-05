d15bdef7c6ebf5112db28220caa162451cc25e63
Yuwandee Rathnayake <yuwandee03@gmail.com>
2025-01-05 11:40:14 +0530

    yuwandee

--- Event Mangement System/build/classes/event/mangement/system/EventManagementSystem.rs
+++
@@ -1,5 +0,0 @@
-event.mangement.system.DBConnection
-event.mangement.system.Event
-event.mangement.system.EventMangementSystem
-event.mangement.system.EventManagementSystem
-event.mangement.system.EventDAO
