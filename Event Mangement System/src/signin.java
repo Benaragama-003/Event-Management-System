8cb55669ccb02f3c79cb6dc783616e76caad3e56
Akila Benaragama <akilabenaragama@gmail.com>
2025-01-05 10:55:42 +0530

    m

--- Event Mangement System/src/signin.java
+++ Event Mangement System/src/signin.java
@@ -39,6 +39,7 @@ public class signin extends javax.swing.JFrame {
         email_signin = new javax.swing.JTextField();
         password_signin = new javax.swing.JPasswordField();
         signin_button = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
         jScrollPane3 = new javax.swing.JScrollPane();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
@@ -48,7 +49,6 @@ public class signin extends javax.swing.JFrame {
         jPanel1.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
         jPanel1.setMaximumSize(new java.awt.Dimension(800, 650));
         jPanel1.setPreferredSize(new java.awt.Dimension(1220, 768));
-        jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         signuppanel.setBackground(new java.awt.Color(255, 255, 255));
         signuppanel.setPreferredSize(new java.awt.Dimension(1210, 768));
@@ -121,7 +121,24 @@ public class signin extends javax.swing.JFrame {
                 .addContainerGap(228, Short.MAX_VALUE))
         );
 
-        jPanel1.add(signuppanel, new org.netbeans.lib.awtextra.AbsoluteConstraints(610, 0, 610, 770));
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/eventura-high-resolution-logo.png"))); // NOI18N
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 719, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(signuppanel, javax.swing.GroupLayout.PREFERRED_SIZE, 610, javax.swing.GroupLayout.PREFERRED_SIZE))
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addComponent(signuppanel, javax.swing.GroupLayout.PREFERRED_SIZE, 770, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 0, Short.MAX_VALUE)
+                .addGap(10, 10, 10))
+        );
 
         getContentPane().add(jPanel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, -1, -1));
 
@@ -183,6 +200,7 @@ public class signin extends javax.swing.JFrame {
     private javax.swing.ButtonGroup buttonGroup1;
     private javax.swing.JLabel email;
     private javax.swing.JTextField email_signin;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollBar jScrollBar2;
     private javax.swing.JScrollBar jScrollBar3;
